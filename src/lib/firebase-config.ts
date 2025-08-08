13a3b4ba9b98bcb39b0568af3d57c4dc7c709861
Arif Aziz <arif.aziz.25ultra@gmail.com>
2025-08-08 07:42:49 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/lib/firebase-config.ts
@@ -0,0 +1,10 @@
+// This configuration is safe to be exposed on the client-side.
+// Firebase security rules are used to protect data.
+export const firebaseConfig = {
+  apiKey: "AIzaSyANglq4YsqN3PP0Sq8-U2L3fa6x0Fh_5lg",
+  authDomain: "marinasuite-mvp.firebaseapp.com",
+  projectId: "marinasuite-mvp",
+  storageBucket: "marinasuite-mvp.appspot.com",
+  messagingSenderId: "125869964378",
+  appId: "1:125869964378:web:d74000f2f1b050c2156c63",
+};
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,9 @@
-
-import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
+import { getApp, getApps, initializeApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, type Firestore } from 'firebase/firestore';
+import { firebaseConfig } from './firebase-config';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyANglq4YsqN3PP0Sq8-U2L3fa6x0Fh_5lg",
-    authDomain: "marinasuite-mvp.firebaseapp.com",
-    projectId: "marinasuite-mvp",
-    storageBucket: "marinasuite-mvp.appspot.com",
-    messagingSenderId: "125869964378",
-    appId: "1:125869964378:web:d74000f2f1b050c2156c63",
-};
-
-// Initialize Firebase
-const app: FirebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app: FirebaseApp = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const auth: Auth = getAuth(app);
 const db: Firestore = getFirestore(app);
 
