0b8a532f303dea4a2a32adb18c37e8a827d9ce54
Arif Aziz <arif.aziz.25ultra@gmail.com>
2025-08-08 07:40:29 +0000

    you are not able to fix the error....FirebaseError: Firebase: Error (aut

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyANglq4YsqN3PP0Sq8-U2L3fa6x0Fh_5lg",
     authDomain: "marinasuite-mvp.firebaseapp.com",
     projectId: "marinasuite-mvp",
-    storageBucket: "marinasuite-mvp.firebasestorage.app",
+    storageBucket: "marinasuite-mvp.appspot.com",
     messagingSenderId: "125869964378",
     appId: "1:125869964378:web:d74000f2f1b050c2156c63",
 };
